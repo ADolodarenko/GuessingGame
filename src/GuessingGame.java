0c9ea3f6c8dfdbcd819f31e5035c47196374a482
ADolodarenko <adolodar@gmail.com>
2018-07-25 23:44:36 +0300

    The initial commit.

---
+++ src/GuessingGame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class GuessingGame extends JFrame {
+	public GuessingGame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Alex's Hi-Lo Guessing Game");
+		getContentPane().setLayout(null);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
